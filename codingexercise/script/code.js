6059a89e9bb02f4f68744d6acd9de5331c8a825c
Jason-Barron07 <129856045+Jason-Barron07@users.noreply.github.com>
2023-05-17 10:59:37 +0200

    Add files via upload

---
+++ codingexercise/script/code.js
@@ -0,0 +1,20 @@
+let person = {
+
+    name:'Jason',
+    age:19
+
+};
+
+let person2 = {
+
+    name:'Chelsey',
+    age:18
+};
+
+console.log(person);
+console.log(person2);
+let people= [person,person2];
+
+console.log(people)
+
+
